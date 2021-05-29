aab1f7c789aa8fed32cc8cacf8b45e24f4ea37ba
Jiwon Park <park@jiwon.me>
2021-05-29 14:54:58 +0900

    autosave

--- HW11/main.c
+++ HW11/main.c
@@ -1,3 +1,10 @@
+/**
+ * Hanyang bus reservation program.
+ * 2021 Jiwon Park
+ * https://github.com/jiwonMe/HY12855_CLanguage
+ * 
+ */
+
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
@@ -10,7 +17,25 @@
 #define PLATFORM_NAME "unknown"
 #endif
 
-int main_menu();
+#define MAX_LENGTH 20
+
+//define boolean type
+typedef enum _boolean
+{
+    FALSE,
+    TRUE
+} boolean;
+
+typedef struct
+{
+    char first_name[MAX_LENGTH];
+    char last_name[MAX_LENGTH];
+    char userID[MAX_LENGTH];
+    char password[MAX_LENGTH];
+} User;
+
+//requirements
+boolean main_menu();
 void registration();
 void login();
 void seat_reservation();
@@ -18,14 +43,18 @@ void seat_cancelation();
 void back_to_main();
 void quit();
 
+//add
+boolean isRegistered(User user);
+void reservation(User user);
+
 int main()
 {
-    while (1)
-        //if 0 is returned, quit programm.
+    while (TRUE)
+        //if FALSE is returned, quit program.
         main_menu() ? 0 : exit(0);
 }
 
-int main_menu()
+boolean main_menu()
 {
     printf("\nWelcome to Hanyang bus reservation!\n\n\n");
     printf("1 : register!!!\n");
@@ -34,6 +63,7 @@ int main_menu()
     printf("Enter your choice :\n");
     int choice;
     scanf("%d", &choice);
+    getchar();
     switch (choice)
     {
     case 1:
@@ -44,11 +74,11 @@ int main_menu()
         break;
     case 3:
         quit();
-        return 0;
+        return FALSE;
     default:
         break;
     }
-    return 1;
+    return TRUE;
 }
 
 void registration()
@@ -57,18 +87,18 @@ void registration()
     char **buffer = (char **)malloc(4 * sizeof(char *)); //input buffer
 
     for (int i = 0; i < 4; i++)
-        buffer[i] = (char *)malloc(100 * sizeof(char)); //allocating memory
+        buffer[i] = (char *)malloc(MAX_LENGTH * sizeof(char)); //allocating memory
 
     getchar(); //cleaning buffer
 
     printf("Enter first name : ");
-    fgets(buffer[0], 100, stdin);
+    fgets(buffer[0], MAX_LENGTH, stdin);
     printf("Enter last name : ");
-    fgets(buffer[1], 100, stdin);
+    fgets(buffer[1], MAX_LENGTH, stdin);
     printf("Enter your Username : ");
-    fgets(buffer[2], 100, stdin);
+    fgets(buffer[2], MAX_LENGTH, stdin);
     printf("Enter your password : ");
-    fgets(buffer[3], 100, stdin);
+    fgets(buffer[3], MAX_LENGTH, stdin);
 
     for (char **ptr = buffer; ptr < buffer + 4; ptr++)
     {
@@ -91,13 +121,18 @@ void registration()
 
 void login()
 {
-    FILE *log = fopen("login.txt", "r");
-    char **buffer = (char **)malloc(2 * sizeof(char *)); //input buffer
+    User *user = (User *)malloc(sizeof(User));
 
     printf("\nUserID : ");
+    fgets(user->userID, MAX_LENGTH, stdin);
     printf("\nPassword : ");
+    fgets(user->password, MAX_LENGTH, stdin);
 
-    printf("\nYour UserID or password is incorrect, please try again :-(");
+    if (isRegistered(*user))
+        registration(user);
+    else
+        printf("\nYour UserID or password is incorrect, please try again :-(");
+    free(user);
     back_to_main();
 }
 
@@ -108,7 +143,7 @@ void quit()
 
 void back_to_main()
 {
-    printf("PLATFORM: %s\n", PLATFORM_NAME);
+    //implementation of 'press any key to continue' by platform.
     printf("press any key to continue...\n");
     if (strcmp(PLATFORM_NAME, "windows") == 0)
         system("PAUSE");
@@ -116,4 +151,29 @@ void back_to_main()
         system("read -n 1 -s -r");
     else
         getchar();
+}
+
+boolean isRegistered(User user)
+{
+    FILE *log = fopen("login.txt", "r");
+    char *line = NULL;
+    size_t len = 0;
+    ssize_t read;
+
+    //parse line by line
+    while ((read = getline(&line, &len, log)) != -1)
+    {
+        char *buff = strtok(line, ", ");
+        User temp;
+        while (buff != NULL)
+        {
+            printf("%s\n", buff);
+            buff = strtok(NULL, ", ");
+        }
+
+        if (strcmp(temp.userID, user.userID) == 0 && strcmp(temp.password, user.password) == 0)
+            return TRUE;
+    }
+
+    return FALSE;
 }
